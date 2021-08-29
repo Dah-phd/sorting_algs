8e6cb24c64b6896120aed4a1e34c75b72c079119
Daniel Nikolaev <toolsproed@gmail.com>
2021-08-29 15:56:09 +0300

    converted to functions, changed functioning

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 mod sorting;
 
 fn main() {
-    let mut vec: Vec<f64> = vec![5.0, 2.0, 3., 54., 5., 6., 7., 8.];
-    let sorted = sorting::BacktrackSort::sort(vec);
+    let vec: Vec<f64> = vec![5.0, 2.0, 3., 54., 5., 6., 7., 8.];
+    let sorted = sorting::bakctracking_sort(&vec);
     println!("{:?}", sorted);
+    println!("Original: {:?}", vec)
 }
--- src/sorting.rs
+++ src/sorting.rs
@@ -1,41 +1,27 @@
-pub struct BacktrackSort<'a, T: Into<f64>> {
-    vec: &'a mut Vec<T>,
+pub fn bakctracking_sort<T: Into<f64> + Copy>(vec: &Vec<T>) -> Vec<T> {
+    let mut vec = vec.to_vec();
+    while !sorting_algorithm(&mut vec, 0, 1) {}
+    vec
 }
-impl<'a, T: Into<f64> + Copy> BacktrackSort<'a, T> {
-    fn sorting_algorithm(&mut self, first_i: usize, second_i: usize) -> bool {
-        if second_i == self.vec.len() {
+
+fn sorting_algorithm<T: Into<f64> + Copy>(
+    vector: &mut Vec<T>,
+    first_i: usize,
+    second_i: usize,
+) -> bool {
+    if second_i == vector.len() {
+        return true;
+    }
+    while vector[first_i].into() <= vector[second_i].into() {
+        if sorting_algorithm(vector, first_i + 1, second_i + 1) {
             return true;
         }
-        while self.vec[first_i].into() <= self.vec[second_i].into() {
-            if self.sorting_algorithm(first_i + 1, second_i + 1) {
-                return true;
-            }
-        }
-        self.shuffle(first_i, second_i);
-        false
     }
-    fn shuffle(&mut self, first_i: usize, second_i: usize) {
-        let save = self.vec[first_i];
-        self.vec[first_i] = self.vec[second_i];
-        self.vec[second_i] = save;
-    }
-    pub fn sort(mut sorted_vec: Vec<T>) -> Vec<T> {
-        let mut sorter = BacktrackSort {
-            vec: &mut sorted_vec,
-        };
-        while !sorter.sorting_algorithm(0, 1) {}
-        sorted_vec
-    }
-    // fn parallel_shuffle(&mut self, first_i: usize, second_i: usize) {
-    //     let save = self.vec[first_i];
-    //     self.vec[first_i] = self.vec[second_i];
-    //     self.vec[second_i] = save;
-    //     for vector in self.parallel_vecs {
-    //         vector
-    //     }
-    // }
-
-    pub fn sort_parallel(mut sorted_vec: Vec<T>) -> Vec<T> {
-        sorted_vec
-    }
+    move_values(vector, first_i, second_i);
+    false
+}
+fn move_values<T: Into<f64> + Copy>(vector: &mut Vec<T>, first_i: usize, second_i: usize) {
+    let save = vector[first_i];
+    vector[first_i] = vector[second_i];
+    vector[second_i] = save;
 }
