baca45371d9558861b8af8887fed003c9e0691e3
Daniel Nikolaev <toolsproed@gmail.com>
2021-09-04 17:35:37 +0300

    third alg, not very effective and complex!

--- src/sorting.rs
+++ src/sorting.rs
@@ -1,5 +1,8 @@
+use std::collections::HashMap;
+
 //ALGORITHMS
 //================================================
+
 // algorithm using backtracking to sort Vec, by checking condition between two values and moving on
 fn sorting_backtrack<T: Into<f64> + Copy>(
     vector: &mut Vec<T>,
@@ -36,8 +39,41 @@ fn sorting_recursive<T: Into<f64> + Copy>(vector: &mut Vec<T>, first_i: usize, s
     }
 }
 
+// point by all comperison with recursion
+fn onetoall_recursive<T: Into<f64> + Copy>(
+    vector: &Vec<T>,
+    position: usize,
+    positions: &mut HashMap<usize, usize>,
+) {
+    let mut new_position: usize = 0;
+    for i in 0..vector.len() {
+        if vector[i].into() < vector[position].into() {
+            new_position += 1;
+        }
+    }
+    positions.insert(new_position, position);
+    if position < vector.len() - 1 {
+        onetoall_recursive(vector, position + 1, positions)
+    }
+}
+
 // TRIGGER FUNCTION
 //================================================
+pub fn onetoallrec_sort<T: Into<f64> + Copy>(vec: &Vec<T>) -> (Vec<T>, HashMap<usize, usize>) {
+    let mut hash = HashMap::new();
+    onetoall_recursive(vec, 0, &mut hash);
+    let mut vector = Vec::new();
+    for i in 0..vec.len() {
+        let val = hash.get(&i);
+        if val.is_none() {
+            vector.push(vector[i - 1])
+        } else {
+            vector.push(vec[*val.unwrap()])
+        }
+    }
+    (vector, hash)
+}
+
 pub fn bakctracking_sort<T: Into<f64> + Copy>(vec: &Vec<T>) -> Vec<T> {
     let mut vec = vec.to_vec();
     while !sorting_backtrack(&mut vec, 0, 1) {}
