337fa6794ba49b9c16aada10b86b53536ddf04a7
Daniel Nikolaev <toolsproed@gmail.com>
2021-09-04 12:16:04 +0300

    added recursive sorting alg

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,13 @@ mod sorting;
 fn main() {
     println!("Using backtracking sort:");
     let mut vec: Vec<f64> = vec![5.0, 2.0, 3., 54., 5., 6., 7., 8.];
-    let sorted = sorting::bakctracking_sort(&vec);
-    println!("{:?}", sorted);
+    let sorted_by_backtrack = sorting::bakctracking_sort(&vec);
+    println!("{:?}", sorted_by_backtrack);
+    println!("Using recursive sort:");
+    let sorted_by_rec = sorting::recursive_sort(&vec);
+    println!("{:?}", sorted_by_rec);
     println!("Original: {:?}", vec);
-    println!("Using backtracking sort inplace:");
-    sorting::bakctracking_sort_inplace(&mut vec);
-    println!("Original: {:?}", vec);
+    // println!("Using backtracking sort inplace:");
+    // sorting::bakctracking_sort_inplace(&mut vec);
+    // println!("Original: {:?}", vec);
 }
--- src/sorting.rs
+++ src/sorting.rs
@@ -1,13 +1,20 @@
 pub fn bakctracking_sort<T: Into<f64> + Copy>(vec: &Vec<T>) -> Vec<T> {
     let mut vec = vec.to_vec();
-    while !sorting_algorithm(&mut vec, 0, 1) {}
+    while !sorting_backtrack(&mut vec, 0, 1) {}
     vec
 }
 pub fn bakctracking_sort_inplace<T: Into<f64> + Copy>(vec: &mut Vec<T>) {
-    while !sorting_algorithm(vec, 0, 1) {}
+    while !sorting_backtrack(vec, 0, 1) {}
+}
+pub fn recursive_sort<T: Into<f64> + Copy>(vec: &Vec<T>) -> Vec<T> {
+    let mut vec = vec.to_vec();
+    sorting_recursive(&mut vec, 0, 1);
+    vec
 }
 
-fn sorting_algorithm<T: Into<f64> + Copy>(
+pub fn backtracking_parallel_sort() {}
+
+fn sorting_backtrack<T: Into<f64> + Copy>(
     vector: &mut Vec<T>,
     first_i: usize,
     second_i: usize,
@@ -16,13 +23,29 @@ fn sorting_algorithm<T: Into<f64> + Copy>(
         return true;
     }
     while vector[first_i].into() <= vector[second_i].into() {
-        if sorting_algorithm(vector, first_i + 1, second_i + 1) {
+        if sorting_backtrack(vector, first_i + 1, second_i + 1) {
             return true;
         }
     }
     move_values(vector, first_i, second_i);
     false
 }
+
+fn sorting_recursive<T: Into<f64> + Copy>(vector: &mut Vec<T>, first_i: usize, second_i: usize) {
+    if vector[first_i].into() <= vector[second_i].into() {
+        if second_i < vector.len() - 1 {
+            sorting_recursive(vector, first_i + 1, second_i + 1)
+        }
+    } else {
+        move_values(vector, first_i, second_i);
+        if first_i != 0 {
+            sorting_recursive(vector, first_i - 1, second_i - 1)
+        } else {
+            sorting_recursive(vector, first_i + 1, second_i + 1)
+        }
+    }
+}
+
 fn move_values<T: Into<f64> + Copy>(vector: &mut Vec<T>, first_i: usize, second_i: usize) {
     let save = vector[first_i];
     vector[first_i] = vector[second_i];
